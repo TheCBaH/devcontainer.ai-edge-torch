507d164e0a9a275988b87e631461a8b0da8ab575
Vladimir N. Silyaev <vsilyaev@gmail.com>
2025-03-21 06:34:46 +0000

    fixup! fixup! devcontainer tidbits for transformers

--- .devcontainer/features/pip-requirements/install.sh
+++ .devcontainer/features/pip-requirements/install.sh
@@ -4,6 +4,9 @@ set -x
 
 echo "Activating feature 'pip_requirements'"
 
-python3 -m pip --no-cache-dir install -r requirements.txt
+python3 -m pip config set global.break-system-packages true
+python3 -m pip --no-cache-dir install -r requirements.txt \
+ --index-url https://download.pytorch.org/whl/cpu \
+ --extra-index-url https://pypi.python.org/simple \
 
 python3 -m pip --no-cache-dir install torch ai-edge-model-explorer
